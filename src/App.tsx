ba8466a6dd269f39ea5124523ebf9fb5aa67a50a
Amelia <nawrotamelia3@gmail.com>
2024-10-05 00:50:48 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Login from './Auth/Login';
+import Register from './Auth/Register';
 
 function About() {
     return <h2>About Page</h2>;
@@ -12,7 +13,8 @@ function App() {
    <Router>
        <Routes>
            <Route path="/" element={<Home />} />
-           <Route path="/login" element={<Login />} />
+           <Route path="/Auth/login" element={<Login />} />
+           <Route path="/Auth/register" element={<Register/>}/>
        </Routes>
    </Router>
 
